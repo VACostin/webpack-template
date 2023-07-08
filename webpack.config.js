fd7ed2b5ca4bab1ce2e08302f09c8422dd525382
Costin <virlan.costin.andrei@gmail.com>
2023-07-09 00:12:34 +0300

    Added production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.jsx"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html"
-    })
-  ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource"
-      },
-      {
-        test: /\.(?:js|jsx|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-        resolve: {
-          extensions: [".js", ".jsx"]
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      index: "./src/index.jsx"
+    },
+    devServer: {
+      static: "./dist"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html"
+      })
+    ],
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource"
+        },
+        {
+          test: /\.(?:js|jsx|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+          resolve: {
+            extensions: [".js", ".jsx"]
+          }
         }
+      ]
+    },
+    resolve: {
+      alias: {
+        Slices: path.resolve(__dirname, "src/slices"),
+        Styles: path.resolve(__dirname, "src/styles")
       }
-    ]
-  },
-  resolve: {
-    alias: {
-      Alias1: path.resolve(__dirname, "src/whatever1"),
-      Alias2: path.resolve(__dirname, "src/whatever2")
     }
-  }
+  };
+  if (argv.mode === "development") {
+    config.devtool = "inline-source-map";
+    config.mode = "development";
+  } else config.mode = "production";
+  return config;
 };
